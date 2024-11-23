d360d10d23b9a8ac6521bcbbb31b592c85cce58a
Richard Murray <rfm@yorku.ca>
2024-11-22 19:05:00 -0500

    added null cube

--- appendix b/make_cubes.m
+++ appendix b/make_cubes.m
@@ -1,10 +1,11 @@
-% make_cube.m
+% make_cube.m  Make a few cube files
 
 clear; clc;
 
 addpath tools
 
 t = TonemapCube;
+
 t_knot = t.u_knot .^ 2;
 t.setchannels(t_knot);
 t.save('square.cube');
@@ -17,3 +18,7 @@ t_knot = zeros(size(t.u_knot));
 t_knot(16) = 1;
 t.setchannels(t_knot);
 t.save('delta_16.cube');
+
+t_knot = t.u_knot;
+t.setchannels(t_knot);
+t.save('none.cube');
