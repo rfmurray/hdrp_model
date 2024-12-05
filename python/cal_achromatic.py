b9e73c1621c3419308d44ffa0312cf143f05bc00
Richard Murray <rfm@yorku.ca>
2024-12-05 18:42:48 -0500

    wrote calibration demo

---
+++ python/cal_achromatic.py
@@ -0,0 +1,45 @@
+# cal_achromatic.py
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import optimize
+from hdrp import srgb, gamma, gammainv, TonemapCube
+
+grey = np.array([0, 5, 10, 20, 30, 40, 50, 100, 125, 150, 175, 200, 225, 255])
+v_k = grey/255
+
+lum = 5 + 95*(v_k**2.2) + np.random.normal(size=grey.shape)
+
+def errfn(param):
+    return ((lum - gamma(v_k, *param))**2).sum()
+pinit = np.array((max(lum)-min(lum), 0, 2.2, min(lum)))
+cons = optimize.LinearConstraint(np.array((0,1,0,0)).reshape((1,4)), np.array((0,)))
+r = optimize.minimize(errfn, pinit, constraints=cons)
+phat = r.x
+print(r)
+
+xx = np.linspace(0,1,1000)
+plt.plot(xx, gamma(xx, *phat), 'k-')
+plt.plot(v_k, lum, 'ro', markersize=10)
+plt.xlabel('unprocessed v_k', fontsize=18)
+plt.ylabel('luminance (cd/m$^2$)', fontsize=18)
+plt.show()
+
+# def h(v_k):
+#     return gamma(v_k, *phat)
+#
+# def hstar(v_k):
+#     return gamma(v_k, *phat) / gamma(1, *phat)
+
+def hstarinv(lumstar):
+    return gammainv(lumstar*gamma(1,*phat), *phat)  # check this
+
+def f(u_k):
+    return srgb(hstarinv(u_k))
+
+tonemap = TonemapCube()
+t_knot = f(tonemap.u_knot)
+tonemap.setchannels(t_knot)
+tonemap.save('linearize1.cube')
+
+# optimize t_knot
---
+++ unity projects/caldemo/Assets/MainScript.cs
@@ -0,0 +1,164 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+public class MainScript : MonoBehaviour
+{
+    // phases of the experiment; used to save state between calls to Update()
+    enum ProgramPhase { ShowStimulus, GetResponse, Calibrate }
+    ProgramPhase phase = ProgramPhase.ShowStimulus;
+
+    [Header("Check for chromatic calibration; uncheck for achromatic")]
+    public bool chromaticCalibration;  // flag whether to run chromatic or achromatic calibration
+    [Header("Links to objects in the scene")]
+    public GameObject stimulusObject;  // capsule object for orientation discrimination task
+    public GameObject calPlane;        // large plane that will show calibration stimuli
+    public Material calMaterial;       // unlit material of calibration plane
+    public Volume globalVolume;        // global volume object that contains tonemapping object
+    Tonemapping tonemap;               // tonemapping object
+
+    List<Color> calibrationList = new List<Color>();
+
+    void Start()
+    {
+        // seed rng
+        int rngseed = (int)System.DateTime.Now.Ticks;
+        Random.InitState(rngseed);
+
+        // get tonemapping object
+        VolumeProfile globalVolumeProfile = globalVolume.sharedProfile;
+        if (globalVolumeProfile.TryGet<Tonemapping>(out var t))
+            tonemap = t;
+        else
+            Debug.Log("unable to get tonemapping object");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))  // turn tonemapping on
+            SetTonemapping(true);
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))  // turn tonemapping off
+            SetTonemapping(false);
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))  // toggle calibration
+            SetCalibration(phase != ProgramPhase.Calibrate);
+
+        if (Input.GetKeyDown(KeyCode.Q))       // quit experiment
+            Quit();
+
+        if (phase == ProgramPhase.ShowStimulus)      // show a new stimulus
+            ShowNextStimulus();
+        else if (phase == ProgramPhase.GetResponse)  // check for observer's keypress response to stimulus
+            CheckStimulusResponse();
+        else if (phase == ProgramPhase.Calibrate)    // check for keypress in calibration phase
+            CheckCalibrationResponse();
+
+    }
+
+    // ----- functions for stimulus-response phase of experiment -----
+
+    void ShowNextStimulus()
+    {
+        float theta = Random.Range(-30f, 30f);
+        stimulusObject.transform.rotation = Quaternion.Euler(0f, 0f, -theta);
+        phase = ProgramPhase.GetResponse;
+        Debug.Log("next stimulus shown");
+    }
+
+    void CheckStimulusResponse()
+    {
+        if (Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.J))
+        {
+            phase = ProgramPhase.ShowStimulus;
+            Debug.Log("response recorded");
+        }
+    }
+
+    // ----- functions for calibration -----
+
+    void SetCalibration(bool on)
+    {
+        calPlane.SetActive(on);
+        if (on)
+        {
+            phase = ProgramPhase.Calibrate;
+            InitCalibrationStimuli();
+            ShowNextCalibrationStimulus();
+            Debug.Log("starting calibration");
+        }
+        else
+        {
+            phase = ProgramPhase.ShowStimulus;
+            Debug.Log("ending calibration");
+        }
+    }
+
+    void InitCalibrationStimuli()
+    {
+        calibrationList.Clear();
+        if(chromaticCalibration)
+        {
+            int n = 9;
+            for (int i = 0; i <= n; i++)
+            {
+                float g = (float)i / (float)n;
+                calibrationList.Add(new Color(g, 0f, 0f));
+                calibrationList.Add(new Color(0f, g, 0f));
+                calibrationList.Add(new Color(0f, 0f, g));
+                calibrationList.Add(new Color(g, g, g));
+            }
+        }
+        else
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                float g = (float)i / 9f;
+                calibrationList.Add(new Color(g, g, g));
+            }
+        }
+    }
+
+    void CheckCalibrationResponse()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!ShowNextCalibrationStimulus())
+            {
+                SetCalibration(false);
+                Debug.Log("calibration completed");
+                return;
+            }
+            Debug.Log("next calibration stimulus shown");
+        }
+    }
+
+    bool ShowNextCalibrationStimulus()
+    {
+        if (calibrationList.Count == 0)
+            return false;
+        calMaterial.color = calibrationList[0];
+        calibrationList.RemoveAt(0);
+        return true;
+    }
+
+    // ----- other functions -----
+
+    void SetTonemapping(bool on)
+    {
+        tonemap.mode.value = on ? TonemappingMode.External : TonemappingMode.None;
+        Debug.Log("tonemapping " + (on ? "on" : "off"));
+    }
+
+    void Quit()
+    {
+        Debug.Log("quitting experiment");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        UnityEngine.Application.Quit();
+#endif
+    }
+
+}
