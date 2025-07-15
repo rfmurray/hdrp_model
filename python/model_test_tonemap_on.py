847e0f29b5f79a269cde40abbfe6b59348b844b9
Richard Murray <rfm@yorku.ca>
2025-07-14 23:34:45 -0400

    changed plot colours

--- python/model_test_tonemap_on.py
+++ python/model_test_tonemap_on.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from hdrp import srgb, srgbinv, TonemapCube, cubetag
 
 # choose whether to test results from Unity project render_random with Lambertian or unlit material
-testLambertian = False
+testLambertian = True
 
 # choose cube files
 cubelist = ['cube/linear_max1.cube', 'cube/square_max1.cube', 'cube/square_root_max1.cube']
@@ -16,10 +16,10 @@ cuben = len(cubelist)
 tonemap = [TonemapCube(f) for f in cubelist]
 
 # knot points estimated from delta functions (Table 2a)
-u_knot = [0, 1e-09, 0.0002606, 0.003104, 0.007305, 0.01288, 0.02056, 0.03061, 0.04468, 0.06393, 0.09056, 0.1245, 0.1712, 0.2354, 0.3236, 0.4406, 0.5938, 0.8165, 1.111, 1.498, 2.039, 2.776, 3.78, 5.094, 6.935, 9.441, 12.72, 17.32, 23.35, 31.78, 43.27, 58.9]
+#u_knot = [0, 1e-09, 0.0002606, 0.003104, 0.007305, 0.01288, 0.02056, 0.03061, 0.04468, 0.06393, 0.09056, 0.1245, 0.1712, 0.2354, 0.3236, 0.4406, 0.5938, 0.8165, 1.111, 1.498, 2.039, 2.776, 3.78, 5.094, 6.935, 9.441, 12.72, 17.32, 23.35, 31.78, 43.27, 58.9]
 
 # knot points estimated from model fitting (Table 2b)
-#u_knot = [0, 1e-09, 1.657e-09, 0.002830, 0.007137, 0.01269, 0.02051, 0.03086, 0.04479, 0.06444, 0.08989, 0.1252, 0.1726, 0.2370, 0.3253, 0.4422, 0.6039, 0.8207, 1.104, 1.495, 2.032, 2.756, 3.738, 5.083, 6.864, 9.347, 12.62, 17.18, 23.24, 31.48, 42.75, 57.66]
+u_knot = [0, 1e-09, 1.657e-09, 0.002830, 0.007137, 0.01269, 0.02051, 0.03086, 0.04479, 0.06444, 0.08989, 0.1252, 0.1726, 0.2370, 0.3253, 0.4422, 0.6039, 0.8207, 1.104, 1.495, 2.032, 2.756, 3.738, 5.083, 6.864, 9.347, 12.62, 17.18, 23.24, 31.48, 42.75, 57.66]
 
 # assign selected knot points to tonemapping objects
 for t in tonemap:
@@ -117,7 +117,7 @@ ax2.set_ylabel('prediction error', fontsize=18)
 ax2.set_xlim(xlim)
 ax2.set_ylim((-0.02, 0.02))
 ax2.set_aspect(1. / ax2.get_data_ratio())
-ax2.text(0.85, -0.012, '(b)', fontsize=24)
+ax2.text(0.80, -0.0165, '(b)', fontsize=24)
 
 plt.savefig(f'figures/model_test_tonemap_on_L{int(testLambertian)}.pdf', bbox_inches='tight')
 plt.show()
